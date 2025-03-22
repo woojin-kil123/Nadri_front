6862965ad00afecd34bf358446ac15748f2271f7
Woojin-kil <dnwls9013@naver.com>
2025-03-22 15:16:05 +0900

    로그인 후 헤더 추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,22 @@
 import "./default.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
-import { useState } from "react";
+import { use, useState } from "react";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import DirectionsIcon from "@mui/icons-material/Directions";
+import Box from "@mui/material/Box";
+import Badge from "@mui/material/Badge";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+
 const Header = () => {
+  const [loginId, setLoginId] = useState("user01");
   return (
     <header className="header">
       <div>
@@ -16,7 +24,7 @@ const Header = () => {
           <Link to="/">NADRI</Link>
         </div>
         <MainNavi></MainNavi>
-        <HeaderLink />
+        <HeaderLink loginId={loginId} />
       </div>
     </header>
   );
@@ -44,14 +52,25 @@ const MainNavi = () => {
   );
 };
 const HeaderLink = (props) => {
+  const loginId = props.loginId;
   return (
     <ul className="user-menu">
-      <li>
-        <Link to="/login">로그인</Link>
-      </li>
-      <li>
-        <Link to="/join">회원가입</Link>
-      </li>
+      {loginId ? (
+        <>
+          <li>
+            <Icon></Icon>
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            <Link to="/login">로그인</Link>
+          </li>
+          <li>
+            <Link to="/join">회원가입</Link>
+          </li>
+        </>
+      )}
     </ul>
   );
 };
@@ -77,5 +96,44 @@ const CustomizedInputBase = () => {
         <DirectionsIcon />
       </IconButton>
     </Paper>
+  );
+};
+const Icon = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const menuId = "primary-search-account-menu";
+  const handleProfileMenuOpen = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  return (
+    <>
+      <Box sx={{ display: { xs: "none", md: "flex" } }}>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <IconButton
+          size="large"
+          edge="end"
+          aria-label="account of current user"
+          aria-controls={menuId}
+          aria-haspopup="true"
+          onClick={handleProfileMenuOpen}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+      </Box>
+    </>
   );
 };
