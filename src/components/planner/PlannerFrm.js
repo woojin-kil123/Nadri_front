8ec6a02ffa99acfc1a347f1d0507689f9e99363c
Newtypeman-1 <dlfgdjqjfls@gmail.com>
2025-03-25 17:13:11 +0900

    카카오맵 api 설치

--- src/components/planner/PlannerFrm.js
+++ src/components/planner/PlannerFrm.js
@@ -1,7 +1,17 @@
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+
 const PlannerFrm = () => {
   return (
     <section className="section">
       <div className="page-title">플래너</div>
+      <Map
+        center={{ lat: 33.5563, lng: 126.79581 }}
+        style={{ width: "100%", height: "360px" }}
+      >
+        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+          <div style={{ color: "#000" }}>Hello World!</div>
+        </MapMarker>
+      </Map>
     </section>
   );
 };
