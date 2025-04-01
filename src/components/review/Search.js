5653e618af2d113f3eaae6d2f439ccd72ea7c9d3
leesangminn <tkdals9909@naver.com>
2025-04-01 10:37:24 +0900

    feat:리뷰를 위한 검색 기능 추가

--- src/components/review/Search.js
+++ src/components/review/Search.js
@@ -1,8 +1,45 @@
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import "./review.css";
 const Search = () => {
   return (
     <section className="section">
       <div className="page-title">어떤 리뷰를 작성하시겠습니까?</div>
+      <div className="search">
+        <CustomizedInputBase />
+      </div>
     </section>
   );
 };
+const CustomizedInputBase = () => {
+  return (
+    <Paper
+      component="form"
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 600 }}
+    >
+      <IconButton sx={{ p: "10px" }} aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="리뷰 작성하실 곳을 검색해 주세요"
+        inputProps={{ "aria-label": "search google maps" }}
+      />
+      <IconButton
+        type="button"
+        sx={{ p: "10px" }}
+        aria-label="search"
+        onClick={() => {
+          console.log("hi");
+        }}
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
 export default Search;
