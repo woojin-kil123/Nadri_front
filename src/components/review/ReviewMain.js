6d03758e8dee1d75e9afc089185292fb0cbd2424
leesangminn <tkdals9909@naver.com>
2025-04-02 13:28:58 +0900

    feat:페이지 네비게이트 생성및  클레스이름 변경으로 css변경 axios로 데이터 변경

--- src/components/review/ReviewMain.js
+++ src/components/review/ReviewMain.js
@@ -2,29 +2,38 @@ import { useEffect, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { RecoilState, useRecoilValue } from "recoil";
 import "./review.css";
-import { colors } from "@mui/material";
 import axios from "axios";
-import ReviewWrite from "./ReviewWrite";
+import PageNavigation from "../utils/PageNavigtion";
 const ReviewMain = () => {
   const backServer = process.env.REACT_APP_BACK_SERVER;
-  const [Review, setReview] = useState([]);
+  const [review, setReview] = useState([]);
   const [pi, setPi] = useState([]);
   const [reqPage, setReqPage] = useState(1);
   const [content, setContnet] = useState(null);
   const chaneContnent = (value) => {
     setContnet(value);
-    console.log(value);
+    axios
+      .get(
+        `${process.env.REACT_APP_BACK_SERVER}/review?reqPage=${reqPage}&value=${value}`
+      )
+      .then((res) => {
+        console.log(res);
+        setReview(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <section className="section">
       <div className="page-title">최신리뷰</div>
-      <div className="nav-wrap">
-        <nav className="nav">
+      <div className="review-nav-wrap">
+        <nav className="review-nav">
           <ul>
             <li
               style={{ width: "15%" }}
               onClick={() => {
-                chaneContnent("숙소");
+                chaneContnent("room");
               }}
             >
               숙소
@@ -32,15 +41,15 @@ const ReviewMain = () => {
             <li
               style={{ width: "15%" }}
               onClick={() => {
-                chaneContnent("여행지");
+                chaneContnent("spot");
               }}
             >
-              여행지
+              관광지
             </li>
             <li
               style={{ width: "15%" }}
               onClick={() => {
-                chaneContnent("음식");
+                chaneContnent("food");
               }}
             >
               음식
@@ -48,7 +57,7 @@ const ReviewMain = () => {
             <li
               style={{ width: "15%" }}
               onClick={() => {
-                chaneContnent("즐길거리");
+                chaneContnent("todo");
               }}
             >
               즐길거리
@@ -56,7 +65,7 @@ const ReviewMain = () => {
             <li
               style={{ width: "15%" }}
               onClick={() => {
-                chaneContnent("플래너");
+                chaneContnent("plan");
               }}
             >
               플래너
@@ -69,31 +78,14 @@ const ReviewMain = () => {
           </Link>
         </div>
       </div>
-      <div className="contnent">
-        <ReviewList data={content} />
+      <div className="contnent-review-list">
+        <div>{content}</div>
+        <div className="review-paging-wrap">
+          <PageNavigation pi={pi} reqPage={reqPage} setReqPage={setReqPage} />
+        </div>
       </div>
     </section>
   );
 };
-const ReviewList = (props) => {
-  const content = props.data;
-  /* useEffect(
-    axios
-      .get()
-      .then((res) => {
-        console.log(res);
-        setReview(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      }),
 
-    []
-  );*/
-  return (
-    <section className="section">
-      <div>{content}</div>
-    </section>
-  );
-};
 export default ReviewMain;
