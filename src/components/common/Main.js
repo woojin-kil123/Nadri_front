5703694c0763b7231eaa02b4c8f0f1ce5b96beab
woojin <dnwls9013@gmail.com>
2025-04-02 17:09:45 +0900

    feat : 인기리뷰

--- src/components/common/Main.js
+++ src/components/common/Main.js
@@ -3,6 +3,7 @@ import IntroSlider from "./IntroSlide";
 import { Box, Tab, Tabs, Typography } from "@mui/material";
 import RecommandSlider from "./RecommandSlider";
 import "./main.css";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const Main = () => {
   const planCategory = ["인기", "최신", , "혼자", "단체"];
@@ -11,33 +12,56 @@ const Main = () => {
   return (
     <section className="section main-wrap">
       <IntroSlider />
-      <CategoryBlock title="추천 플랜" categories={planCategory} />
-      <CategoryBlock title="추천 컨텐츠" categories={contentCategory} />
+      <div className="recommand-wrap">
+        <div className="recommand-title">
+          <h2>추천플랜</h2>
+        </div>
+        <div className="recommand-nav">
+          <FilterNavWithPanel categories={planCategory} />
+        </div>
+        <RecommandSlider />
+      </div>
+      <div className="recommand-wrap">
+        <div className="recommand-title">
+          <h2>추천 컨텐츠</h2>
+        </div>
+        <div className="recommand-nav">
+          <FilterNavWithPanel categories={contentCategory} />
+        </div>
+        <RecommandSlider />
+      </div>
+      <div className="recommand-wrap">
+        <div className="recommand-title">
+          <h2>인기 리뷰</h2>
+        </div>
+        <div className="recommand-nav">
+          <FilterNavWithPanel categories={contentCategory} />
+        </div>
+        <div className="hot-review-wrap">
+          <div className="main-review">
+            <ReviewCard />
+          </div>
+          <div className="other-review-wrap">
+            <div className="other-review">
+              <ReviewCard />
+              <ReviewCard />
+            </div>
+            <div className="other-review">
+              <ReviewCard />
+              <ReviewCard />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
-  );
-};
-
-const CategoryBlock = ({ title, categories }) => {
-  return (
-    <div className="recommand-wrap">
-      <div className="recommand-title">
-        <h2>{title}</h2>
-      </div>
-      <div className="recommand-nav">
-        <FilterNavWithPanel categories={categories} />
-      </div>
-      <RecommandSlider categories={categories} />
-    </div>
   );
 };
 
 const FilterNavWithPanel = ({ categories }) => {
   const [value, setValue] = useState(0);
-
   const handleChange = (_, newValue) => {
     setValue(newValue);
   };
-
   return (
     <Box
       sx={{
@@ -55,12 +79,6 @@ const FilterNavWithPanel = ({ categories }) => {
           "& .MuiTabs-indicator": {
             backgroundColor: "#27b778", // 여기에서 커스터마이징!
           },
-          "& .MuiTab-root": {
-            color: "#3d3d3d", // 기본 탭 텍스트 색상
-          },
-          "& .Mui-selected": {
-            color: "#27b778",
-          },
         }}
       >
         {categories.map((category, index) => (
@@ -70,4 +88,30 @@ const FilterNavWithPanel = ({ categories }) => {
     </Box>
   );
 };
+const ReviewCard = () => {
+  return (
+    <div
+      className="review-card"
+      style={{
+        backgroundImage: `url(/image/dora.png)`,
+        backgroundSize: "cover",
+        width: "100%",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="heart-icon">
+        <FavoriteIcon style={{ color: "#4CAF50" }} />
+      </div>
+      <div className="card-content">
+        <h3 className="title">남산순환나들길</h3>
+        <div className="rating">
+          <span className="stars">★★★★★</span>
+          <span className="score">5.0 (22)</span>
+        </div>
+        <p className="category">국립공원</p>
+      </div>
+    </div>
+  );
+};
+
 export default Main;
