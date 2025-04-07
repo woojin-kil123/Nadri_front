0804dce3177bdc523fa1b8919c09fbd01c69b96c
woojin <dnwls9013@gmail.com>
2025-04-07 13:43:44 +0900

    feat : 검색창 디자인

--- src/components/common/MainSearch.js
+++ src/components/common/MainSearch.js
@@ -9,31 +9,97 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Menu from "@mui/material/Menu";
 import "./mainSearch.css";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Popper from "@mui/material/Popper";
+import MenuList from "@mui/material/MenuList";
+import Stack from "@mui/material/Stack";
+
 const MainSearch = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
   };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = useRef(open);
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
   return (
-    <Paper
-      component="form"
-      className="main-search"
-      sx={{
-        p: "2px 4px",
-        display: "flex",
-        alignItems: "center",
-        width: 400,
-        justifyContent: "space-between",
-      }}
-    >
-      <IconButton sx={{ p: "10px" }} aria-label="menu">
+    <div className="main-search">
+      <Stack direction="row" spacing={2}>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom-start" ? "left top" : "left bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </Stack>
+      <IconButton
+        sx={{ p: "10px" }}
+        aria-label="menu"
+        ref={anchorRef}
+        id="composition-button"
+        aria-controls={open ? "composition-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+      >
         <MenuIcon />
       </IconButton>
       <Autocomplete
@@ -45,18 +111,34 @@ const MainSearch = () => {
         options={top100Films}
         getOptionLabel={(option) => option.title}
         renderInput={(params) => (
-          <TextField {...params} variant="standard" placeholder="검색" />
+          <TextField
+            {...params}
+            variant="outlined"
+            placeholder="검색"
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: "12px", // 둥근 테두리
+                backgroundColor: "#ffffff", // 배경색
+                "& fieldset": {
+                  borderColor: "#d6d6d6", // 기본 테두리 색
+                },
+                "&:hover fieldset": {
+                  borderColor: "#27b778", // 호버 시 테두리 색
+                  cursor: "pointer",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#27b778", // 포커스 시 테두리 색
+                  borderWidth: "2px",
+                },
+              },
+            }}
+          />
         )}
       />
-      <IconButton
-        type="button"
-        sx={{ p: "10px" }}
-        aria-label="search"
-        onClick={handleClick}
-      >
-        <SearchIcon />
+      <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon sx={{ width: "30px", height: "30px" }} />
       </IconButton>
-    </Paper>
+    </div>
   );
 };
 export default MainSearch;
