09e9a1182a9d839690f3fd55aa3bf20ca8fab64a
hongbee-coder <congbii94@gmail.com>
2025-04-10 13:44:57 +0900

    feat : 상세페이지 구현

--- src/components/place/PlaceDetail.js
+++ src/components/place/PlaceDetail.js
@@ -2,35 +2,63 @@ import { useEffect, useState } from "react";
 import "./place.css";
 import axios from "axios";
 import StarRating from "../utils/StarRating";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Place } from "@mui/icons-material";
 
 const PlaceDetail = () => {
   const backServer = process.env.REACT_APP_BACK_SERVER;
   const [review, setReview] = useState([]);
-  const [pi, setPi] = useState([]);
   const [reqPage, setReqPage] = useState(1);
+  const placeId = useParams().placeId;
+  const [place, setPlace] = useState();
   useEffect(() => {
     axios
-      .get(`${backServer}/review?reqPage=${reqPage}&value=${"all"}`)
+      .get(`${backServer}/place/detail?placeId=${placeId}`)
       .then((res) => {
         console.log(res);
-        setReview(res.data.list);
-        setPi(res.data.pi);
+        setPlace(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get(`${backServer}/review/detail/${placeId}`)
+      .then((res) => {
+        console.log(res);
+        setReview(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, [reqPage]);
   return (
-    <div className="container">
-      <div className="header">
-        <p className="rating">
-          ★ 5.0 (22) <span className="location">서울시, 중구</span>
-        </p>
-        <h1 className="title">남산순환나들길</h1>
+    <div className="place-detail-wrap">
+      <div className="place-detail-header">
+        <div className="detail-header-cat">
+          {place && <div>{place.cat2Name}</div>}
+          {place && <div>{place.cat3Name}</div>}
+        </div>
+        <div className="detail-header-title">
+          {place && <h1 className="detail-title">{place.placeTitle}</h1>}
+        </div>
+        <div className="detail-header-info">
+          {place && <a>{place.areaName},</a>}
+          {place && <a> {place.sigunguName}</a>}
+        </div>
       </div>
-
-      <div className="content">
+      <div className="place-detail-image">
+        {place && (
+          <img
+            src={place.placeThumb ? place.placeThumb : "/image/dora.png"}
+            className="detail-img1"
+          ></img>
+        )}
+        <div className="detail-img2"></div>
+        <div className="detail-img3"></div>
+        <div className="detail-img4"></div>
+      </div>
+      <div className="place-detail-content">
         <div className="main-description">
           <p>
             도시 한복판에 우뚝 솟은 남산은 서울의 랜드마크이자 세계적 시민들의
@@ -48,24 +76,12 @@ const PlaceDetail = () => {
             <strong>문의 및 안내</strong>
             <p>02-3783-5900</p>
           </div>
-          <div className="info-item">
-            <strong>영업시간</strong>
-            <p>상시 개방</p>
-          </div>
-          <div className="info-item">
-            <strong>쉬는날</strong>
-            <p>연중무휴</p>
-          </div>
-          <div className="info-item">
-            <strong>주차시설</strong>
-            <p>가능</p>
-          </div>
         </div>
       </div>
       <div className="place-detail page-title">
         <h2>리뷰</h2>
         <Link to="/review/write" className="btn-primary green">
-          글쓰기
+          리뷰쓰기
         </Link>
       </div>
       <div className="place-detail review-wap">
