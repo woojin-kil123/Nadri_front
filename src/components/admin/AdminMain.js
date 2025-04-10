721af8fba1169a65f86ef1be97c77658c2278e07
woojin <dnwls9013@gmail.com>
2025-04-10 15:17:40 +0900

    feat: 메인 페이지 통계

--- src/components/admin/AdminMain.js
+++ src/components/admin/AdminMain.js
@@ -4,20 +4,45 @@ import { useRecoilValue } from "recoil";
 import { placeTypeState } from "../utils/RecoilData";
 import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
 import { IconButton } from "@mui/material";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const AdminMain = () => {
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
   const placeType = useRecoilValue(placeTypeState);
   const [reviewStat, setReviewStat] = useState(null);
+  const [planStat, setPlanStat] = useState(null);
   const [company, setCompany] = useState(null);
   const [editMode, setEditMode] = useState(false);
   const [isUpdate, setIsUpdate] = useState(false);
+  const [pieChartData, setPieChartData] = useState([]);
+
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_BACK_SERVER}/review/stats`)
       .then((res) => {
-        console.log(res.data);
         setReviewStat(res.data);
+        // PieChart에 맞는 데이터 구조로 변환 후 상태에 저장
+        const pieData = res.data.map((item) => ({
+          name: `${
+            placeType.find((type, _) => item.placeTypeId === type.id)?.name
+          }`,
+          value: item.reviewCount,
+        }));
+        setPieChartData(pieData); // 이게 핵심!!
       });
+  }, []);
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BACK_SERVER}/plan/stats`).then((res) => {
+      console.log(res.data);
+      setPlanStat(res.data);
+    });
   }, []);
   useEffect(() => {
     axios
@@ -43,16 +68,63 @@ const AdminMain = () => {
   };
   return (
     <>
-      <div className="review-stats">
-        <div className="total">
-          <h2>총 통계</h2>
-          <div>
-            <h4>리뷰</h4>
-            {reviewStat.map((review, i) => (
-              <div>
-                {review.placeTypeId}/{review.reviewCount}개
-              </div>
-            ))}
+      <div className="total">
+        <h2>운영 통계</h2>
+        <div className="stats">
+          <div className="total-title">
+            <h3>리뷰</h3>
+          </div>
+          <div className="total-content">
+            <div className="chart" style={{ width: "50%", height: "300px" }}>
+              <ResponsiveContainer>
+                <PieChart>
+                  <Pie
+                    data={pieChartData}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={100}
+                    fill="#8884d8"
+                    dataKey="value"
+                    label={({ name, percent }) =>
+                      `${name} (${(percent * 100).toFixed(0)}%)`
+                    }
+                  >
+                    {pieChartData.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))}
+                  </Pie>
+                  <Tooltip />
+                  <Legend />
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+            <div className="description">
+              <h4>총 리뷰 수</h4>
+              {pieChartData &&
+                pieChartData.map((data, i) => (
+                  <div key={"review-des" + i}>
+                    {data.name} : {data.value}건
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+        <div className="stats">
+          <div className="total-title">
+            <h3>플랜</h3>
+          </div>
+          <div className="total-content">
+            <div className="chart" style={{ width: "50%", height: "300px" }}>
+              <h4>많이 등록된 장소</h4>
+              <div> ㅎㅎㅎㅎ</div>
+            </div>
+            <div className="description">
+              <h4>총 플랜 수</h4>
+              {planStat && <div>{planStat.planCount}건</div>}
+            </div>
           </div>
         </div>
       </div>
