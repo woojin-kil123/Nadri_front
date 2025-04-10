cfd3c087b5d6c8e0bdf43e6de43c05cea7f49306
hongbee-coder <congbii94@gmail.com>
2025-04-10 16:10:24 +0900

    feat : 리스트, 상세페이지 기본 프론트 완

--- src/components/utils/ListCard.js
+++ src/components/utils/ListCard.js
@@ -1,11 +1,17 @@
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./ListCard.css"; // CSS는 따로 작성
 import StarRating from "./StarRating";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function ListCard(props) {
   const navigate = useNavigate();
   const place = props.place;
+  const [liked, setLiked] = useState(false);
+  const handleClick = () => {
+    setLiked((prev) => !prev);
+  };
   return (
     <>
       {!place ? (
@@ -23,11 +29,15 @@ export default function ListCard(props) {
               src={place.placeThumb ? place.placeThumb : "/image/dora.png"}
               className="card-image"
             />
-            <div className="heart-icon">
-              <FavoriteIcon
-                style={{ color: "#4CAF50" }}
-                onClick={place.favorFunc}
-              />
+            <div
+              className="heart-icon"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <div onClick={handleClick} style={{ cursor: "pointer" }}>
+                {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+              </div>
             </div>
           </div>
           <div className="card-content">
@@ -38,11 +48,11 @@ export default function ListCard(props) {
             <h3 className="title">{place.placeTitle}</h3>
             <div className="rating">
               <span className="stars">
-                <StarRating rating={place.rating} />
+                <StarRating rating={place.placeRating} />
               </span>
               <span className="score">
-                {place.rating}
-                {place.ratingCount && `(${place.ratingCount})`}
+                {place.placeRating}
+                {place.placeReview && ` (${place.placeReview})`}
               </span>
             </div>
             <p className="category">{place.cat3Name}</p>
