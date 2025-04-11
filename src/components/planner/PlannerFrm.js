82e6640276d611f8c4d307cf0e420c1df4cb4bce
Newtypeman-1 <dlfgdjqjfls@gmail.com>
2025-04-11 18:06:00 +0900

    [Refactor] clean up unused props/state, and organize code order

--- src/components/planner/PlannerFrm.js
+++ src/components/planner/PlannerFrm.js
@@ -18,7 +18,6 @@ import MarkerWithOverlay from "./MarkerWithOverlay";
 import { useRecoilState } from "recoil";
 import { loginNicknameState } from "../utils/RecoilData";
 import BasicSelect from "../utils/BasicSelect";
-import html2canvas from "html2canvas";
 
 const PlannerFrm = () => {
   //마커 오버레이 여닫음
@@ -31,10 +30,20 @@ const PlannerFrm = () => {
   const [plannedPlaceList, setPlannedPlaceList] = useState([]);
   //플래너 제목
   const [planName, setPlanName] = useState("");
-  //플래너 공개 여부
-  const [planStatus, setPlanStatus] = useState("");
   //플래너 저장 창 여닫음
   const [openSaveModal, setOpenSaveModal] = useState(false);
+  //장소 리스트(임시 데이터)
+  const [placeList, setPlaceList] = useState([]);
+  //정렬 옵션(1:거리순, 2:리뷰많은순, 3:이름순)
+  const [sortOption, setSortOption] = useState(1);
+  //필터 옵션(null:전체, 1:숙박시설, 2:음식점, 3:그외)
+  const [filterOption, setFilterOption] = useState(null);
+  //유저닉네임
+  const [loginNickname, setLoginNickname] = useRecoilState(loginNicknameState);
+  //플래너 소유자(작성자) 여부
+  const [isOwner, setIsOwner] = useState(true);
+
+  //↓ 맵 관련 STATE
   //현재 보이는 지도 화면
   const [mapBounds, setMapBounds] = useState(null);
   //지도 중심좌표(화면 이동 시 사용)
@@ -46,19 +55,16 @@ const PlannerFrm = () => {
   const [userMarker, setUserMarker] = useState(null);
   //유저 클릭 위치를 중심으로 하는 반경 범위
   const [userRadius, setUserRadius] = useState(1000);
-  //장소 리스트(임시 데이터)
-  const [placeList, setPlaceList] = useState([]);
-  //정렬 옵션(1:거리순, 2:리뷰많은순, 3:이름순)
-  const [sortOption, setSortOption] = useState(1);
-  //필터 옵션(null:전체, 1:숙박시설, 2:음식점, 3:그외)
-  const [filterOption, setFilterOption] = useState(null);
-  //유저닉네임
-  const [loginNickname, setLoginNickname] = useRecoilState(loginNicknameState);
+
+  //↓ 플래너 상태 판별용 STATE
+  //주소에서 받은 planNo
+  const { planNo } = useParams();
+  //플래너 모드: view, write, null
+  const [plannerMode, setPlannerMode] = useState(null);
 
   //↓ 작성된 useEffect 목록
 
-  //플래너 진입 시 "새 플래너" 진입인지, "기존 플래너" 진입인지 판단
-  const { planNo } = useParams();
+  //플래너 진입 시 "새 플래너 작성"인지, "기존 플래너 열람"인지 판단
   useEffect(() => {
     if (planNo) {
       getPlanData(planNo);
@@ -108,7 +114,7 @@ const PlannerFrm = () => {
 
   //↓ 함수 및 값
 
-  //작성 완료된 플래너 조회 시
+  //작성된 플래너 조회 시
   const getPlanData = useCallback(() => {
     const refreshToken = window.localStorage.getItem("refreshToken");
     axios
@@ -227,12 +233,6 @@ const PlannerFrm = () => {
     return filteredSortedList;
   }, [placeList, sortOption, filterOption]);
 
-  const markableList = useMemo(() => {
-    return filteredSortedList.filter(
-      (item) => !plannedPlaceList.find((p) => p.placeId === item.placeId)
-    );
-  }, [filteredSortedList, plannedPlaceList]);
-
   //filter 기능 제공 값
   const filterItems = [
     { name: "숙박시설", value: 1 },
@@ -315,13 +315,11 @@ const PlannerFrm = () => {
               <PrintPlaceList
                 key={"place-" + idx}
                 place={p}
-                idx={idx}
                 openPlanningModal={openPlanningModal}
                 setOpenPlanningModal={setOpenPlanningModal}
                 plannedPlaceList={plannedPlaceList}
                 setPlannedPlaceList={setPlannedPlaceList}
                 setOpenPlanner={setOpenPlanner}
-                markableList={markableList}
                 setOpenOverlay={setOpenOverlay}
                 setMapCenter={setMapCenter}
               />
@@ -333,7 +331,6 @@ const PlannerFrm = () => {
         <Planner
           setOpenPlanner={setOpenPlanner}
           plannedPlaceList={plannedPlaceList}
-          setPlannedPlaceList={setPlannedPlaceList}
           handleDeletePlace={handleDeletePlace}
           planName={planName}
           setPlanName={setPlanName}
@@ -379,30 +376,26 @@ const PlannerFrm = () => {
       </div>
       {openSaveModal && (
         <SavePlanModal
+          loginNickname={loginNickname}
           planName={planName}
           setPlanName={setPlanName}
           setOpenSaveModal={setOpenSaveModal}
-          loginNickname={loginNickname}
           plannedPlaceList={plannedPlaceList}
         />
       )}
       <div className="map-wrap">
         <PrintMap
-          filteredSortedList={filteredSortedList}
           openOverlay={openOverlay}
           setOpenOverlay={setOpenOverlay}
-          openPlanningModal={openPlanningModal}
           setOpenPlanningModal={setOpenPlanningModal}
-          mapBounds={mapBounds}
           setMapBounds={setMapBounds}
           userMarker={userMarker}
           setUserMarker={setUserMarker}
           userRadius={userRadius}
-          setUserRadius={setUserRadius}
           plannedPlaceList={plannedPlaceList}
           handleDeletePlace={handleDeletePlace}
-          markableList={markableList}
           mapCenter={mapCenter}
+          filteredSortedList={filteredSortedList}
         />
       </div>
     </div>
@@ -442,7 +435,6 @@ const CustomizedInputBase = () => {
 // 장소 리스트 출력하는 사이드 창
 const PrintPlaceList = (props) => {
   const p = props.place;
-  const idx = props.idx;
   const [openPlanningModal, setOpenPlanningModal] = [
     props.openPlanningModal,
     props.setOpenPlanningModal,
@@ -452,7 +444,6 @@ const PrintPlaceList = (props) => {
     props.setPlannedPlaceList,
   ];
   const setOpenPlanner = props.setOpenPlanner;
-  const markableList = props.markableList;
   const setOpenOverlay = props.setOpenOverlay;
   const setMapCenter = props.setMapCenter;
 
@@ -488,7 +479,6 @@ const PrintPlaceList = (props) => {
       </div>
       {openPlanningModal === p.placeId && (
         <PlanningModal
-          openPlanningModal={openPlanningModal}
           setOpenPlanningModal={setOpenPlanningModal}
           place={p}
           plannedPlaceList={plannedPlaceList}
@@ -503,15 +493,9 @@ const PrintPlaceList = (props) => {
 // 여행 플래너 출력 창
 const Planner = (props) => {
   const setOpenPlanner = props.setOpenPlanner;
-  const [plannedPlaceList, setPlannedPlaceList] = [
-    props.plannedPlaceList,
-    props.setPlannedPlaceList,
-  ];
+  const plannedPlaceList = props.plannedPlaceList;
   const handleDeletePlace = props.handleDeletePlace;
   const [planName, setPlanName] = [props.planName, props.setPlanName];
-  const handlePlanName = (e) => {
-    setPlanName(e.target.value);
-  };
 
   return (
     <div className="planner-wrap">
@@ -576,19 +560,16 @@ const Planner = (props) => {
 
 // "여행지에 추가하기" 모달 창
 const PlanningModal = (props) => {
-  const [openPlanningModal, setOpenPlanningModal] = [
-    props.openPlanningModal,
-    props.setOpenPlanningModal,
-  ];
+  const setOpenPlanningModal = props.setOpenPlanningModal;
   const p = props.place;
   const [plannedPlaceList, setPlannedPlaceList] = [
     props.plannedPlaceList,
     props.setPlannedPlaceList,
   ];
+  const setOpenPlanner = props.setOpenPlanner;
 
   const now = dayjs();
   const [date, setDate] = useState(now);
-  // console.log(date.format("YYYY-MM-DD"));
   const [transport, setTransport] = useState("");
   const [order, setOrder] = useState(plannedPlaceList.length);
 
@@ -621,7 +602,6 @@ const PlanningModal = (props) => {
     setOpenPlanner(true);
   };
 
-  const setOpenPlanner = props.setOpenPlanner;
   return (
     <div className="modal-background">
       <div className="planning-modal">
@@ -787,25 +767,27 @@ const SavePlanModal = (props) => {
 
 //카카오맵
 const PrintMap = (props) => {
-  const filteredSortedList = props.filteredSortedList;
   const [openOverlay, setOpenOverlay] = [
     props.openOverlay,
     props.setOpenOverlay,
   ];
-  const [openPlanningModal, setOpenPlanningModal] = [
-    props.openPlanningModal,
-    props.setOpenPlanningModal,
-  ];
-  const [mapBounds, setMapBounds] = [props.mapBounds, props.setMapBounds];
+  const setOpenPlanningModal = props.setOpenPlanningModal;
+  const setMapBounds = props.setMapBounds;
   const [userMarker, setUserMarker] = [props.userMarker, props.setUserMarker];
-  const [userRadius, setUserRadius] = [props.userRadius, props.setUserRadius];
+  const userRadius = props.userRadius;
   const plannedPlaceList = props.plannedPlaceList;
   const handleDeletePlace = props.handleDeletePlace;
-  const markableList = props.markableList;
   const mapCenter = props.mapCenter;
+  const filteredSortedList = props.filteredSortedList;
+
+  const markableList = useMemo(() => {
+    return filteredSortedList.filter(
+      (item) => !plannedPlaceList.find((p) => p.placeId === item.placeId)
+    );
+  }, [filteredSortedList, plannedPlaceList]);
 
   return (
-    <Map // 지도를 표시할 Container
+    <Map
       id={`kakaomap`}
       center={mapCenter}
       style={{
@@ -825,11 +807,6 @@ const PrintMap = (props) => {
         } else {
           setOpenOverlay(null);
         }
-      }}
-      //지도 로드 완료 시
-      onCreate={(map) => {
-        //지도 중심 좌표
-        // console.log(map.getCenter());
       }}
       //현재 보이는 화면 범위를 가져옴
       onBoundsChanged={(map) => {
@@ -895,23 +872,21 @@ const PrintMap = (props) => {
           />
         </>
       )}
-      {markableList.map((p, idx) => {
+      {markableList.map((p) => {
         return (
           <MarkerWithOverlay
             key={"marker-" + p.placeId}
             place={p}
-            idx={idx}
             openOverlay={openOverlay}
             setOpenOverlay={setOpenOverlay}
             setOpenPlanningModal={setOpenPlanningModal}
           />
         );
       })}
-      {plannedPlaceList.map((p, idx) => (
+      {plannedPlaceList.map((p) => (
         <MarkerWithOverlay
           key={"planned-" + p.placeId}
           place={p}
-          idx={markableList.length + idx}
           openOverlay={openOverlay}
           setOpenOverlay={setOpenOverlay}
           isPlanned={true}
