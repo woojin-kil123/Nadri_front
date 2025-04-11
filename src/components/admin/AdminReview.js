dbeeb074bb5b93a6d21bb94938918f750cbe78d6
woojin <dnwls9013@gmail.com>
2025-04-11 22:37:26 +0900

    refactor : 리뷰 카테고리 셀렉트 태그로 수정

--- src/components/admin/AdminReview.js
+++ src/components/admin/AdminReview.js
@@ -2,7 +2,15 @@ import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { placeTypeState } from "../utils/RecoilData";
 import axios from "axios";
-import { Box, Tab, Tabs } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Tab,
+  Tabs,
+} from "@mui/material";
 
 const AdminReview = () => {
   const placeType = useRecoilValue(placeTypeState);
@@ -11,12 +19,12 @@ const AdminReview = () => {
   const [reportedReview, setReportedReview] = useState(null);
   const [isUpdate, setIsUpdate] = useState(false);
   useEffect(() => {
+    if (!selectedType) return;
     axios
       .get(
         `${process.env.REACT_APP_BACK_SERVER}/review/hotReview?type=${selectedType}`
       )
       .then((res) => {
-        console.log(res.data);
         setHotReview(res.data);
       });
   }, [selectedType]);
@@ -24,19 +32,31 @@ const AdminReview = () => {
     axios
       .get(`${process.env.REACT_APP_BACK_SERVER}/admin/report`)
       .then((res) => {
-        console.log(res.data);
         setReportedReview(res.data);
       });
   }, [isUpdate]);
+  const handleChange = (event) => {
+    setSelectedType(event.target.value);
+  };
   return (
     <>
-      <FilterNavWithPanel
-        categories={placeType}
-        on={selectedType}
-        setOn={setSelectedType}
-      />
       <div className="hot-review-wrap">
-        <h3>인기 리뷰</h3>
+        <div className="hot-review-title">
+          <h3>인기 리뷰</h3>
+          <Select
+            notched={false}
+            labelId="hot-review-label"
+            id="hot-review"
+            value={selectedType}
+            onChange={handleChange}
+          >
+            {placeType.map((type, i) => (
+              <MenuItem key={"type-" + i} value={type.id}>
+                {type.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
         <table className="hot-review tbl">
           <thead>
             <tr>
@@ -87,33 +107,3 @@ const AdminReview = () => {
   );
 };
 export default AdminReview;
-
-const FilterNavWithPanel = ({ categories, on, setOn }) => {
-  const placeType = useRecoilValue(placeTypeState);
-  const tabIndex = categories.findIndex((type) => type.id === on); // 문자열인 on → index 변환
-
-  const handleChange = (_, newIndex) => {
-    const selected = categories[newIndex].id;
-    setOn(selected);
-  };
-
-  return (
-    <Box sx={{ width: "100%", bgcolor: "background.paper", p: 1 }}>
-      <Tabs
-        value={tabIndex === -1 ? 0 : tabIndex}
-        onChange={handleChange}
-        aria-label="category tabs"
-        indicatorColor="primary"
-        sx={{
-          "& .MuiTabs-indicator": {
-            backgroundColor: "#27b778",
-          },
-        }}
-      >
-        {categories.map((category, i) => (
-          <Tab key={category.id + "-" + i} label={category.name} />
-        ))}
-      </Tabs>
-    </Box>
-  );
-};
