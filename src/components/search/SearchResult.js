4252b1a0e1f29a3c74f2cafe78c08c23800c5bc0
woojin <dnwls9013@gmail.com>
2025-04-14 10:20:47 +0900

    feat: 검색 로직 조정

--- src/components/search/SearchResult.js
+++ src/components/search/SearchResult.js
@@ -3,6 +3,7 @@ import { useRecoilValue } from "recoil";
 import { placeTypeState } from "../utils/RecoilData";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Box, Tab, Tabs } from "@mui/material";
 
 const SearchResult = () => {
   const location = useLocation();
@@ -10,26 +11,66 @@ const SearchResult = () => {
   const [query, setQuery] = useState("");
   const [typeId, setTypeId] = useState([]);
   const placeType = useRecoilValue(placeTypeState);
+  const contentType = [
+    { id: 1, name: "여행 정보" },
+    { id: 2, name: "플랜" },
+    { id: 3, name: "리뷰" },
+  ];
+  const [onContent, setOnContent] = useState("");
   useEffect(() => {
     setQuery(queryParams.get("query"));
     setTypeId(queryParams.getAll("type"));
   }, [location]);
   useEffect(() => {
-    axios.get(
-      `${process.env.REACT_APP_BACK_SERVER}/search?query=${query}&type=${placeType[0].id}`
-    );
+    axios
+      .get(
+        `${process.env.REACT_APP_BACK_SERVER}/search?query=${query}&type=${placeType[0].id}`
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
   }, [query, typeId]);
   return (
     <>
       <h1>{query ? `${query}` : ""} 검색결과</h1>
       <section className="section">
-        {typeId.map((type, i) => (
-          <div key={`type${i}`} className="search-result">
-            <h2>{placeType.find((item, i) => item.id == type)?.name}</h2>
-          </div>
-        ))}
+        <FilterNavWithPanel
+          categories={contentType}
+          on={onContent}
+          setOn={setOnContent}
+        />
+        <div className="result-wrap"></div>
       </section>
     </>
   );
 };
 export default SearchResult;
+
+const FilterNavWithPanel = ({ categories, on, setOn }) => {
+  const placeType = useRecoilValue(placeTypeState);
+  const tabIndex = categories.findIndex((type) => type.id === on); // 문자열인 on → index 변환
+  const handleChange = (_, newIndex) => {
+    const selected = categories[newIndex].id;
+    setOn(selected);
+  };
+
+  return (
+    <Box sx={{ width: "100%", bgcolor: "background.paper", p: 1 }}>
+      <Tabs
+        value={tabIndex === -1 ? 0 : tabIndex}
+        onChange={handleChange}
+        aria-label="category tabs"
+        indicatorColor="primary"
+        sx={{
+          "& .MuiTabs-indicator": {
+            backgroundColor: "#27b778",
+          },
+        }}
+      >
+        {categories.map((category, i) => (
+          <Tab key={category.id + "-" + i} label={category.name} />
+        ))}
+      </Tabs>
+    </Box>
+  );
+};
